c7aa686dfbf99a4fa278d56b8a12b9161283f552
almondtools <mandelst@gmail.com>
2017-09-18 06:06:26 +0200

    version update

--- src/main/java/net/amygdalum/gildedroseweb/AgentConfig.java
+++ src/main/java/net/amygdalum/gildedroseweb/AgentConfig.java
@@ -6,6 +6,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 import net.amygdalum.testrecorder.DefaultTestRecorderAgentConfig;
+import net.amygdalum.testrecorder.Packages;
 import net.amygdalum.testrecorder.ScheduledTestGenerator;
 import net.amygdalum.testrecorder.SnapshotConsumer;
 
@@ -27,8 +28,8 @@ public class AgentConfig extends DefaultTestRecorderAgentConfig {
 	}
 
 	@Override
-	public List<String> getPackages() {
-		return asList("com.gildedrose");
+	public List<Packages> getPackages() {
+		return asList(Packages.byName("com.gildedrose"));
 	}
 	
 }
