43a73e54d35e1f4292066ea5f001eeeac6e02812
almondtools <mandelst@gmail.com>
2017-11-28 20:20:14 +0100

    version update 3.9-SNAPSHOT

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import net.amygdalum.testrecorder.Recorded;
+
 class GildedRose {
     Item[] items;
 
@@ -7,6 +9,7 @@ class GildedRose {
         this.items = items;
     }
 
+    @Recorded
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
