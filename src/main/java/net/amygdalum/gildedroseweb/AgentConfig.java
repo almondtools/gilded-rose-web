789d95db2bc0ed321d32039effa0f01c26103a16
almondtools <mandelst@gmail.com>
2018-03-11 23:12:39 +0100

    version update

--- src/main/java/net/amygdalum/gildedroseweb/AgentConfig.java
+++ src/main/java/net/amygdalum/gildedroseweb/AgentConfig.java
@@ -2,34 +2,16 @@ package net.amygdalum.gildedroseweb;
 
 import static java.util.Arrays.asList;
 
-import java.nio.file.Paths;
 import java.util.List;
 
-import net.amygdalum.testrecorder.DefaultTestRecorderAgentConfig;
-import net.amygdalum.testrecorder.ScheduledTestGenerator;
-import net.amygdalum.testrecorder.SnapshotConsumer;
+import net.amygdalum.testrecorder.DefaultSerializationProfile;
 import net.amygdalum.testrecorder.profile.Classes;
 
-public class AgentConfig extends DefaultTestRecorderAgentConfig {
-
-	@Override
-	public SnapshotConsumer getSnapshotConsumer() {
-		return new ScheduledTestGenerator()
-			.withDumpMaximum(1000)
-			.withDumpOnShutDown(true)
-			.withDumpOnCounterInterval(5)
-			.withClassName("${class}${counter}Test")
-			.withDumpTo(Paths.get("target/generated"));
-	}
-	
-	@Override
-	public long getTimeoutInMillis() {
-		return 100_000;
-	}
+public class AgentConfig extends DefaultSerializationProfile {
 
 	@Override
 	public List<Classes> getClasses() {
 		return asList(Classes.byPackage("com.gildedrose"));
 	}
-	
+
 }
